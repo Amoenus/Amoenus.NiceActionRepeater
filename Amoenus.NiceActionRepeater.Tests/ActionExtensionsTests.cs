bd0bff3a3166cfd09fd022d048634a2e1d8060e0
Romans Pokrovskis <romans.pokrovskis@agepartnership.com>
2017-12-08 14:27:14 +0000

    More tests

--- Amoenus.NiceActionRepeater.Tests/ActionExtensionsTests.cs
+++ Amoenus.NiceActionRepeater.Tests/ActionExtensionsTests.cs
@@ -8,16 +8,51 @@ namespace Amoenus.NiceActionRepeater.Tests
     public class ActionExtensionsTests
     {
         [Test]
-        public void RepeatAction_InvokesActionExpectedNumberOfTimes()
+        public void RepeatAction_WhenPositive_InvokesActionExpectedNumberOfTimes()
         {
-            var actualCounter = 0;
-            var expectedCounter = 200;
+            int actualCounter = 0;
+            const int expectedCounter = 200;
 
             Action actionToRepeat = () => { actualCounter++; };
 
             actionToRepeat.RepeatAction(expectedCounter);
 
             expectedCounter.ShouldBeEquivalentTo(actualCounter);
+        }
+
+        [Test]
+        public void RepeatAction_WhenZero_InvokesActionExpectedNumberOfTimes()
+        {
+            int actualCounter = 0;
+            const int expectedCounter = 0;
+
+            Action actionToRepeat = () => { actualCounter++; };
+
+            actionToRepeat.RepeatAction(expectedCounter);
+
+            expectedCounter.ShouldBeEquivalentTo(actualCounter);
+        }
+
+        [Test]
+        public void RepeatAction_WhenNegative_ThrowsArgumentOutOfRangeException()
+        {
+            int actualCounter = 0;
+            const int expectedCounter = -200;
+
+            Action actionToRepeat = () => { actualCounter++; };
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => actionToRepeat.RepeatAction(expectedCounter));
+
+        }
+
+        [Test]
+        public void RepeatAction_WhenNull_ThrowsNullReferenceException()
+        {
+            const int expectedCounter = 200;
+
+            Action actionToRepeat = null;
+
+            Assert.Throws<NullReferenceException>(() => actionToRepeat.RepeatAction(expectedCounter));
         }
     }
 }
