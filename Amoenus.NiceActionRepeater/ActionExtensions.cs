a30cf8cf9972da9ef5c714c1c7ee743879315109
Romans Pokrovskis <romans.pokrovskis@agepartnership.com>
2017-12-08 11:59:25 +0000

    Added solution

---
+++ Amoenus.NiceActionRepeater/ActionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Amoenus.NiceActionRepeater
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public static class ActionExtensions
+    {
+        /// <summary>
+        /// Repeats the action specified number of times.
+        /// </summary>
+        /// <param name="actionToRepeat">The action to repeat.</param>
+        /// <param name="repeatCount">The repeat count.</param>
+        public static void RepeatAction(this Action actionToRepeat, int repeatCount)
+        {
+            foreach (Action action in Enumerable.Repeat(actionToRepeat, repeatCount))
+            {
+                action();
+            }
+        }
+    }
+}
